74148baae56f63be4e1da99f7e00b8366a8505fb
MantasS0 <man_tas_pats@yahoo.com>
2018-11-08 16:58:50 +0200

    Pavyzdys baigtas Pakeista kad issisaugotu duomenys ijungiant app per nauja.

--- app/src/main/java/com/example/ms/roomwordsample/WordRoomDatabase.java
+++ app/src/main/java/com/example/ms/roomwordsample/WordRoomDatabase.java
@@ -1,12 +1,10 @@
 package com.example.ms.roomwordsample;
 
-import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
 import android.os.AsyncTask;
-import android.support.annotation.NonNull;
 
 @Database(entities = {Word.class}, version = 1)
 public abstract class WordRoomDatabase extends RoomDatabase {
@@ -21,7 +19,7 @@ public abstract class WordRoomDatabase extends RoomDatabase {
                     // Create database here
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
                             WordRoomDatabase.class, "word_database")
-                            .addCallback(sRoomDatabaseCallback)
+//                            .addCallback(sRoomDatabaseCallback)
                             .build();
                 }
             }
@@ -29,15 +27,15 @@ public abstract class WordRoomDatabase extends RoomDatabase {
         return INSTANCE;
     }
 
-    private static RoomDatabase.Callback sRoomDatabaseCallback =
-            new RoomDatabase.Callback(){
-
-                @Override
-                public void onOpen (@NonNull SupportSQLiteDatabase db){
-                    super.onOpen(db);
-                    new PopulateDbAsync(INSTANCE).execute();
-                }
-            };
+//    private static RoomDatabase.Callback sRoomDatabaseCallback =
+//            new RoomDatabase.Callback(){
+//
+//                @Override
+//                public void onOpen (@NonNull SupportSQLiteDatabase db){
+//                    super.onOpen(db);
+//                    new PopulateDbAsync(INSTANCE).execute();
+//                }
+//            };
 
     private static class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
 
